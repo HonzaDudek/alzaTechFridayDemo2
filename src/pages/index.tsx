2d2191f3b390ece10852d2c5768d3469482509fa
Honza <honza.dudek@centrum.cz>
2021-06-18 15:23:12 +0200

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,8 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            This is demo for Alza Tech Friday
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
           <SocialList />
         </div>
       </div>
